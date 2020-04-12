f937dc72de3944a384da04cc1971a72b76fd81a7
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-12 16:26:40 -0600

    hello function

--- main.go
+++ main.go
@@ -7,18 +7,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func home(res http.ResponseWriter, req *http.Request){
+func home(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("content-Type", "text/html")
 	fmt.Fprint(res, "<h1>Nathaniel Wheeler</h1>")
 }
 
-func contact(res http.ResponseWriter, req *http.Request){
+func contact(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("content-Type", "text/html")
-	fmt.Fprint(res, 
-		"To get in touch, please send an email "+
-		" to <a href=\"mailto:contact@nathanielwheeler.com\">"+
-		"contact@nathanielwheeler.com</a>."
-	)
+	fmt.Fprint(res,
+		"To get in touch, please send an email to <a href=\"mailto:contact@nathanielwheeler.com\">contact@nathanielwheeler.com</a>.")
 }
 
 func main() {
