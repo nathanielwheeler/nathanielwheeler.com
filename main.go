8f131ee536973cf47e0fec66566a651f541086f2
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-12 16:06:24 -0600

    Using gorilla/mux for routing

--- main.go
+++ main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(res http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-
-	res.Header().Set("Content-Type", "text/html")
-	switch path {
-	case "/":
-		fmt.Fprint(res, "<h1>Nathaniel Wheeler</h1>")
-	case "/contact":
-		fmt.Fprint(res, "To get in touch, please send an email "+
-		" to <a href=\"mailto:contact@nathanielwheeler.com\">"+
-		"contact@nathanielwheeler.com</a>.")
-	default:
-		fmt.Fprint(res, "<h1>Page Not Found</h1>"+
-		"<p>Please email me if you keep being sent to an invalid page.</p>")
-	}
+func home(res http.ResponseWriter, req *http.Request){
+	res.Header().Set("content-Type", "text/html")
+	fmt.Fprint(res, "<h1>Nathaniel Wheeler</h1>")
 }
 
+func contact(res http.ResponseWriter, req *http.Request){
+	res.Header().Set("content-Type", "text/html")
+	fmt.Fprint(res, 
+		"To get in touch, please send an email "+
+		" to <a href=\"mailto:contact@nathanielwheeler.com\">"+
+		"contact@nathanielwheeler.com</a>."
+	)
+}
+
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	router := mux.NewRouter()
+	r.HandleFnc("/", home)
+	r.HandleFnc("/contact", contact)
 	http.ListenAndServe(":3000", nil)
 }
