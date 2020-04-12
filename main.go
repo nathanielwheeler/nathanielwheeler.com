9cc0adf646b9a96f2857f0996bd51f750931cd32
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-12 15:38:04 -0600

    Basic routing

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 )
 
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "<h1>Le Gestionnaire de Photos</h1>")
+	path := req.URL.Path
+
+	res.Header().Set("Content-Type", "text/html")
+	switch path {
+	case "/":
+		fmt.Fprint(res, "<h1>Nathaniel Wheeler</h1>")
+	case "/contact":
+		fmt.Fprint(res, "To get in touch, please send an email "+
+		" to <a href=\"mailto:contact@nathanielwheeler.com\">"+
+		"contact@nathanielwheeler.com</a>.")
+	}
 }
 
 func main() {
