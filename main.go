97eac0deceee4f4d02c4a0304b5cf7c55a4d4786
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-13 14:54:23 -0600

    Added signup form, changed layout

--- main.go
+++ main.go
@@ -8,7 +8,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView, contactView *views.View
+// #region TODO Page adding procedure:
+/*
+
+/views
+- create view template (.html)
+- define as "yield"
+
+/views/layouts/navbar.html
+- Add to navbar
+
+main.go
+- add view variable (*views.View)
+- add handler func
+- in main()...
+	- initialize view
+	- call handler from router
+
+*/
+// #endregion
+
+var (
+	homeView,
+	contactView,
+	signupView *views.View
+)
+
+// #region Handlers
 
 func home(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("content-Type", "text/html")
@@ -20,18 +46,27 @@ func contact(res http.ResponseWriter, req *http.Request) {
 	must(contactView.Render(res, nil))
 }
 
+func signup(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(res, nil))
+}
+
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// #endregion
+
 func main() {
 	homeView = views.NewView("app", "views/home.html")
 	contactView = views.NewView("app", "views/contact.html")
+	signupView = views.NewView("app", "views/signup.html")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", router)
 }
