8f5e89b9567d2e5a3a2843624a23bccc89ac156d
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-13 16:45:27 -0600

    form schema

--- controllers/subscribers.go
+++ controllers/subscribers.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
+
 	"nathanielwheeler.com/views"
 )
 
@@ -12,21 +14,31 @@ type Subscribers struct {
 	NewView *views.View
 }
 
+// SubscribeForm :
+type SubscribeForm struct {
+	Email string `schema: "email"`
+}
+
 // New : GET /subscribe
 // — Renders a new form view for a potential subscriber
 func (sub *Subscribers) New(res http.ResponseWriter, req *http.Request) {
-	err := sub.NewView.Render(res, nil)
-	if err != nil {
+	if err := sub.NewView.Render(res, nil); err != nil {
 		// TODO don't panic && give feedback to subscriber
 		panic(err)
 	}
+
+	decoder := schema.NewDecoder()
+	form := SubscribeForm{}
+	if err := decoder.Decode(&form, req.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(res, form)
 }
 
 // Create : POST /subscribe
 // — Used to process the subscription form when a user tries to subscribe
 func (sub *Subscribers) Create(res http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
+	if err := req.ParseForm(); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(res, req.PostForm["email"])
