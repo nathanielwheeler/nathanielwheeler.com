785e67349ebbdcbea0eb73c847600f647287044b
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-04-15 16:10:55 -0600

    environment checker

--- exp/main.go
+++ exp/main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
+	_ "database/sql"
 	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type env struct {
+	host, user, password, port, name string
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
+	}
+}
+
 func main() {
-	t, err := template.ParseFiles("hello.html")
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println(getEnv())
+}
 
-	data := struct {
-		Name string
-	}{Name: "Nathaniel Wheeler"}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
+func getEnv() env {
+	dbEnv := env{
+		host: checkEnv("host"),
+		user: checkEnv("user"),
+		password: checkEnv("password"),
+		port: checkEnv("port"),
+		name: checkEnv("name"),
 	}
+	return dbEnv
+}
+
+func checkEnv(str string) string {
+	str, exists := os.LookupEnv("DB_" + strings.ToUpper(str))
+	if !exists {
+		panic(".env not properly configured")
+	}
+	return str
 }
