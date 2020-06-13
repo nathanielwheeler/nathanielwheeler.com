4e69446b7a5c64931dca7e3e35863676910f30db
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-06-13 01:01:55 -0600

    remember token validators

--- models/users.go
+++ models/users.go
@@ -35,6 +35,11 @@ var (
 	ErrPasswordTooShort = errors.New("models: password was too short")
 	// TODO password validator for max length
 	// TODO password validator for restricted characters in password
+
+	// ErrRememberRequired is returned when a create or update is attempted without a token hash
+	ErrRememberRequired = errors.New("models: remember token required")
+	// ErrRememberTooShort is returned when a remember token is not at least 32 bytes
+	ErrRememberTooShort = errors.New("models: remember token should be at least 32 bytes")
 )
 
 // TODO: remove obvious pepper when deployed
@@ -271,7 +276,9 @@ func (uv *userValidator) Create(user *User) error {
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
 		uv.setRememberIfUnset,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -288,7 +295,9 @@ func (uv *userValidator) Update(user *User) error {
 		uv.passwordMinLength,
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -446,6 +455,29 @@ func (uv *userValidator) passwordRequired(user *User) error {
 func (uv *userValidator) passwordHashRequired(user *User) error {
 	if user.PasswordHash == "" {
 		return ErrPasswordRequired
+	}
+	return nil
+}
+
+// rememberMinBytes will check if remember token has 32 bytes
+func (uv *userValidator) rememberMinBytes(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	n, err := rand.NBytes(user.Remember)
+	if err != nil {
+		return err
+	}
+	if n < 32 {
+		return ErrRememberTooShort
+	}
+	return nil
+}
+
+// rememberHashRequired checks if there is a remember token hash
+func (uv *userValidator) rememberHashRequired(user *User) error {
+	if user.RememberHash == "" {
+		return ErrRememberRequired
 	}
 	return nil
 }
