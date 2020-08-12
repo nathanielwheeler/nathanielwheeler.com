bcfa049056654f335c225d6f77d7769eb5ce02fe
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-08-12 15:12:35 -0600

    config refactor for yaml

--- config.go
+++ config.go
@@ -1,93 +1,83 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
+	"fmt"
+	"os"
 
-  "github.com/joho/godotenv"
+	"gopkg.in/yaml.v2"
 )
 
-// #region Database
+const (
+	devConfigFile  = ".dev_config.yml"
+	prodConfigFile = ".prod_config.yml"
 
-// PostgresConfig holds database connection info.
-type PostgresConfig struct {
-  Host     string `json:"host"`
-  Port     string `json:"port"`
-  User     string `json:"user"`
-  Password string `json:"password"`
-  DBName   string `json:"name"`
-}
-
-// DefaultPostgresConfig will return development database information
-func DefaultPostgresConfig() PostgresConfig {
-  return PostgresConfig{
-    Host:     checkDBEnv("host"),
-    User:     checkDBEnv("user"),
-    Password: checkDBEnv("password"),
-    Port:     checkDBEnv("port"),
-    DBName:   checkDBEnv("name"),
-  }
-}
-
-// Dialect will return the dialect that GORM will use.
-func (c PostgresConfig) Dialect() string {
-  return "postgres"
-}
-
-// ConnectionString will return a string used to connect to the database
-func (c PostgresConfig) ConnectionString() string {
-  if c.Password == "" {
-    return fmt.Sprintf(
-      "host=%s port=%s user=%s dbname=%s sslmode=disable",
-      c.Host, c.Port, c.User, c.DBName,
-    )
-  }
-  return fmt.Sprintf(
-    "host=%s port=%s user=%s password=%s, dbname=%s sslmode=disable",
-    c.Host, c.Port, c.User, c.Password, c.DBName,
-  )
-}
-
-// CheckForDotEnv will check to see if there are any .Env files.  If there are not, it will panic.
-// TODO Refactor for production
-func CheckForDotEnv() {
-  if err := godotenv.Load(); err != nil {
-    panic("No .env file found")
-  }
-}
-
-func checkDBEnv(str string) string {
-  str, exists := os.LookupEnv("DB_" + strings.ToUpper(str))
-  if !exists {
-    panic(".env is missing environment variable: '" + str + "'")
-  }
-  return str
-}
-
-// #endregion
+	dialect = "postgres"
+)
 
 // Config holds configuration variables
 type Config struct {
-  Port      int    `json:"port"`
-  Env       string `json:"env"`
-  CSRFBytes int    `json:"csrf_bytes"`
-  Pepper    string `json:"pepper"`
-  HMACKey   string `json:"hmcac_key"`
+	Env       string         `yaml:"env"`
+	Port      int            `yaml:"port"`
+	Pepper    string         `yaml:"pepper"`
+	HMACKey   string         `yaml:"hmcac_key"`
+	CSRFBytes int            `yaml:"csrf_bytes"`
+	Database  PostgresConfig `yaml:"database"`
 }
 
-// DefaultConfig sets up Config for a development environment
-func DefaultConfig() Config {
-  return Config{
-    Port:      3000,
-    Env:       "dev",
-    CSRFBytes: 32,
-    Pepper:    "secret-string",
-    HMACKey:   "secret-hmac-key",
+// LoadConfig will load production or development configuration files.
+func LoadConfig(configReq bool) Config {
+	f, err := os.Open(prodConfigFile)
+	if err != nil {
+    if configReq {
+      panic(err)
+    }
+		f, err = os.Open(devConfigFile)
+		if err != nil {
+			panic("No configuration file detected!")
+		}
+		fmt.Println("Using DEVELOPMENT configuration.")
+	} else {
+		fmt.Println("Using PRODUCTION configuration.")
   }
+  defer f.Close()
+	// Decode file and return Config struct
+  var c Config
+  d := yaml.NewDecoder(f)
+	if err := d.Decode(&c); err != nil {
+    panic(err)
+  }
+	return c
 }
 
 // IsProd sets Config Env to Production
 func (c Config) IsProd() bool {
-  return c.Env == "prod"
+	return c.Env == "prod"
+}
+
+// PostgresConfig holds database connection info.
+type PostgresConfig struct {
+	DBName   string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+}
+
+// Dialect will return the dialect that GORM will use.
+func (c PostgresConfig) Dialect() string {
+	return dialect
+}
+
+// ConnectionString will return a string used to connect to the database
+func (c PostgresConfig) ConnectionString() string {
+	if c.Password == "" {
+		return fmt.Sprintf(
+			"host=%s port=%s user=%s dbname=%s sslmode=disable",
+			c.Host, c.Port, c.User, c.DBName,
+		)
+	}
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s, dbname=%s sslmode=disable",
+		c.Host, c.Port, c.User, c.Password, c.DBName,
+	)
 }
