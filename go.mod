7dc16e15d2bf236762dcb15d6805f270ead70d30
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-09-04 15:24:27 -0600

    release script

--- go.mod
+++ go.mod
@@ -7,8 +7,7 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/schema v1.1.0
 	github.com/jinzhu/gorm v1.9.16
-	github.com/joho/godotenv v1.3.0
-	github.com/lib/pq v1.8.0
+	github.com/lib/pq v1.8.0 // indirect
 	github.com/yuin/goldmark v1.2.1
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 	gopkg.in/yaml.v2 v2.3.0
---
+++ scripts/release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd "$GOPATH/src/nathanielwheeler.com"
+echo "==== Releasing nathanielwheeler.com ===="
+
+# Pull current code from master
+echo "	Pulling from master in production..."
+ssh root@128.199.11.200 "cd /root/go/src/nathanielwheeler.com; \
+	git pull"
+
+# Update dependencies
+echo "	Updating dependencies..."
+ssh root@128.199.11.200 "cd /root/go/src/nathanielwheeler.com; \
+	go get -u; \
+	go mod tidy"
+
+# Building binaries
+echo "	Building nathanielwheeler.com..."
+ssh root@128.199.11.200 "cd /root/app; \
+	go build -o ./server /root/go/src/nathanielwheeler.com/*.go"
+
+# Transferring files
+echo "	Transferring files..."
+ssh root@128.199.11.200 "cd /root/app; \
+	cp -R /root/go/src/nathanielwheeler.com/public .; \
+	cp -R /root/go/src/nathanielwheeler.com/views .; \
+	cp /root/go/src/nathanielwheeler.com/Caddyfile ."
+
+# Restarting server
+echo "	Restarting server..."
+ssh root@128.199.11.200 "service nathanielwheeler.com restart"
+
+# Restarting server
+echo "	Restarting Caddy..."
+ssh root@128.199.11.200 "service caddy restart"
+
+echo "==== nathanielwheeler.com Released! ===="
