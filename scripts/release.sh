dc2ca28034b134916cd95d0e0b4e46d5f6f092fb
Nathaniel Wheeler <nathan@mailftp.com>
2020-09-12 17:36:03 -0600

    old nav layout but with right-aligned elements kept

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,11 @@
 cd "$GOPATH/src/nathanielwheeler.com"
 echo "==== Releasing nathanielwheeler.com ===="
 
+# Merge dev into master
+echo "	Merging dev to master..."
+git checkout master
+git merge dev
+
 # Pull current code from master
 echo "	Pulling from master in production..."
 ssh root@128.199.11.200 "cd /root/go/src/nathanielwheeler.com; \
@@ -35,3 +40,6 @@ echo "	Restarting Caddy..."
 ssh root@128.199.11.200 "service caddy restart"
 
 echo "==== nathanielwheeler.com Released! ===="
+
+# Checkout back to dev
+git checkout dev
