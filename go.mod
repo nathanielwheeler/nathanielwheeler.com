69a3fdcb01f534b28b90df545bc7d5500129d30b
Nathaniel Wheeler <nathan@mailftp.com>
2020-12-03 09:48:59 -0700

    parseMarkdown method

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/jackc/pgtype v1.6.1
 	github.com/jackc/pgx/v4 v4.9.2
 	github.com/sirupsen/logrus v1.7.0
+	github.com/yuin/goldmark v1.2.1
+	github.com/yuin/goldmark-meta v1.0.0
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/yaml.v2 v2.3.0
--- server/parse.go
+++ server/parse.go
@@ -1,9 +1,15 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"path/filepath"
+
+	"github.com/yuin/goldmark"
+	meta "github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/parser"
 )
 
 func (s *server) parseTemplates(files ...string) (tpl *template.Template, err error) {
@@ -30,5 +36,41 @@ func (s *server) parseData(data interface{}) interface{} {
 		return nil
 	default:
 		return data
+	}
+}
+
+type markdown struct {
+	Body     *bytes.Buffer
+	MetaData map[string]interface{}
+}
+
+// parseMarkdown will take in a markdown file location and return html
+func (s *server) parseMarkdown(file string) markdown {
+	var (
+		buf bytes.Buffer
+		err error
+	)
+	file = file + ".md"
+
+	src, err := ioutil.ReadFile(file)
+	if err != nil {
+		s.logErr("failed to read .md file", err)
+	}
+
+	md := goldmark.New(
+		goldmark.WithExtensions(
+			meta.Meta,
+		),
+	)
+
+	ctx := parser.NewContext()
+	err = md.Convert([]byte(src), &buf, parser.WithContext(ctx))
+	if err != nil {
+		s.logErr("markdown failed to parse", err)
+	}
+
+	return markdown{
+		Body:     &buf,
+		MetaData: meta.Get(ctx),
 	}
 }
